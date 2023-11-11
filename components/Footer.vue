a8ffef769239ed4d93d616386384bd3be542f141
Christian Tong <christian.tongcruz96@gmail.com>
2023-11-11 12:40:20 -0500

    Products

---
+++ components/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="FooterMain" class="w-full bg-accent-baby-powder mt-10 mb-2">
+    <div class="max-w-[1200px] mx-auto px-4">
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-secondary-rich-black">
+            Lorem ipsum
+          </div>
+          <div class="text-secondary-silver text-sm">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit sem, faucibus
+            risus magnis vitae interdum habitant hendrerit luctus convallis,
+          </div>
+        </div>
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-secondary-rich-black">
+            Habitasse porta inceptos.
+          </div>
+          <div class="text-secondary-silver text-sm">
+            Arcu mus molestie ultricies cras vivamus suscipit purus, nostra a
+            potenti litora integer commodo habitasse, enim facilisis erat
+            facilisi aliquam sagittis.
+          </div>
+        </div>
+      </div>
+
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-secondary-rich-black">
+            Montes proin quis suspendisse
+          </div>
+          <div class="text-secondary-silver text-sm">
+            Varius natoque laoreet class pharetra parturient risus molestie
+            ligula, fermentum mi felis gravida
+          </div>
+        </div>
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-secondary-rich-black">
+            Alibaba Group
+          </div>
+          <div class="text-secondary-silver text-sm">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="FooterSub" class="w-full bg-accent-gunmetal text-sm text-gray-400">
+    <div class="max-w-[1200px] mx-auto p-4">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum
+    </div>
+  </div>
+</template>
+
+<script setup></script>
